62473fb016fbc992307ca0dfa859766e6f63df40
tomot813 <tomot813tom@gmail.com>
2024-12-10 19:02:08 +0300

    blyat

--- js/singIn.js
+++ js/singIn.js
@@ -1,24 +1,53 @@
 
+let singin  = document.forms.form_one;
 
-function singIn(){
+
+singin.addEventListener('submit', (event)=>{
+        event.preventDefault();
     let login = document.getElementById("login_input").value;
     let date = document.getElementById("date_input").value;
     let sex = document.getElementById("sex").value;
 
-    document.getElementById("erorr_output").innerText = "";
+    let erorr = document.getElementsByClassName('erorr_output');
 
-    if(login != "" && date != ""){
+    
+        for (i = 0; i < erorr.length; i++) {
+            erorr[i].style.opacity = 0;
+        }
+
+
+    if((singin.elements.login_input.validity.valid && singin.elements.date_input.validity.valid) && (singin.elements.login_input.value.length >= 4 && singin.elements.login_input.value.length <= 10)){
         sessionStorage.setItem("login", login);
         sessionStorage.setItem("date",date);
         sessionStorage.setItem("sex", sex);
         sessionStorage.setItem("result", 0);
     
         window.location.href = "../index.html"
-    }else{
-        document.getElementById("erorr_output").innerText = "You're not finished";
+    }
+    if (!singin.elements.login_input.validity.valid || !(singin.elements.login_input.value.length >= 4 && singin.elements.login_input.value.length <= 10)){
+        erorr[0].style.opacity = 1;
+
+    }
+    if(!singin.elements.date_input.validity.valid){
+
+        erorr[1].style.opacity = 1;
     }
 
 
-}
+});
 
-document.getElementById("btn_singIn").addEventListener('click', singIn);
+// document.getElementById("btn_singIn").addEventListener('click', singIn);
+
+
+
+// else if((login == "" && date == "") || (singin.elements.login_input.validity.valid && singin.elements.date_input.validity.valid)){
+//         erorr[1].style.opacity = 1;
+//         erorr[0].style.opacity = 1;
+
+//     }else if (date == ""){
+//         erorr[1].style.opacity = 1;
+        
+//     }else if (login == "" || singin.elements.login_input.validity.valid){
+//         erorr[0].style.opacity = 1;
+        
+//     }
